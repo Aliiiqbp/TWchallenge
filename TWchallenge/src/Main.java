ee60a1e647956af7a69b5c7ee252e6ac2eac4a12
Aliiiqbp <aliiiqbp@gmail.com>
2018-10-25 15:16:43 +0330

    ok baba, taze shoroo shode :))

--- TWchallenge/src/Main.java
+++ TWchallenge/src/Main.java
@@ -60,28 +60,44 @@ public class Main {
             }
 
 
-//Add, Remove, Upgrade
+//Add Bazaar, Army ,Defense
             if (command.equalsIgnoreCase("Add Bazar [B.id]")){
 
                 continue;
-            }   else if(command.equalsIgnoreCase("Upgrade [B.id] [U.id]")){
+            }   else if(command.equalsIgnoreCase("Add Army [B.id]")){
 
                 continue;
-            }   else if (command.equalsIgnoreCase("remove"))
+            }   else if (command.equalsIgnoreCase("Add Defense [B.id]")){
+
+                continue;
+            }
+
+
+
+//Upgrade [B.id][U,id]
+            if (command.equalsIgnoreCase("upgrade [B.id] [U.id]")){
+
+                //army, defense, bazaar getType()
+
+                continue;
+            }   else if (command.equalsIgnoreCase("remove [B.id] [U.id]")){
+
+                //army, defense, bazaar getType()
+
+                continue;
+            }
+
+
+
+
+
+
+
+
 
 
         }
-
-
-
-
-
-
-
-
-
-        }
-
+    }
 
 
 
