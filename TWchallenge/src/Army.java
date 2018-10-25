3cb702f07a04913b33c98da674ab134abd67da65
ali <alialaee98@gmail.com>
2018-10-25 18:50:10 +0330

    Home class initiated :)

--- TWchallenge/src/Army.java
+++ TWchallenge/src/Army.java
@@ -1,3 +1,5 @@
+
+
 public class Army {
     private int blockID;
     private int coefficiency;
--- TWchallenge/src/Home.java
+++ TWchallenge/src/Home.java
@@ -1,2 +1,32 @@
 public class Home {
+    private int level = 0;
+    private int population = 0;
+    private int floor = 0;
+    private int unit = 0;
+    private int unitID = 0;
+    private int score = 0;
+    private int gill = 0;
+    private int blockID = 0;
+
+    Home( int flr, int unt, int blckID, int untID){
+        floor = flr;
+        unit = unt;
+        blockID = blckID;
+        unitID = untID;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getGill() {
+        return gill;
+    }
+
+    public int getUnitID() {
+        return unitID;
+    }
+    public void upgradeHome(){
+
+    }
 }
