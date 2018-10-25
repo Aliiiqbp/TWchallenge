950c3332a1b56d9ae3c5d63cd55744b039fe234c
Aliiiqbp <aliiiqbp@gmail.com>
2018-10-25 14:56:01 +0330

    ok baba, taze shoroo shode :))

--- TWchallenge/src/Main.java
+++ TWchallenge/src/Main.java
@@ -11,31 +11,63 @@ public class Main {
 
             String command = read.nextLine();
 
+//End Game *_*
             if (command.equalsIgnoreCase("Yield")){
                 break;
             }
 
 
+//Other commands like score, gill, done, attackloot.
             if (command.equalsIgnoreCase("Done")){
 
                 continue;
-            } else if (command.equalsIgnoreCase("See Score")){
+            }   else if (command.equalsIgnoreCase("See Score")){
 
                continue;
-            } else if (command.equalsIgnoreCase("See Gills")){
+            }   else if (command.equalsIgnoreCase("See Gills")){
 
                 continue;
-            } else if (command.equalsIgnoreCase("Attack []")){
+            }   else if (command.equalsIgnoreCase("Attack [B.id]")){
 
                 continue;
-            } else if (command.equalsIgnoreCase("Loot []")){
+            }   else if (command.equalsIgnoreCase("Loot [B.id]")){
 
                 continue;
             }
 
 
 
+//Add & Upgrade home
+            if (command.equalsIgnoreCase("Add Home [B.id] [numberOfFloor] [NumberOfUnit]")){
 
+                continue;
+            }   else if (command.equalsIgnoreCase("Upgrade [B.id] [U.id] (floor || unit)")){
+
+                continue;
+            }
+
+
+//Add, Remove, Upgrade Block.
+            if (command.equalsIgnoreCase("Add Block")){
+
+                continue;
+            }   else if (command.equalsIgnoreCase("Remove [B.id]")){
+
+                continue;
+            }   else if(command.equalsIgnoreCase("Upgrade [B.id]")){
+
+                continue;
+            }
+
+
+//Add, Remove, Upgrade
+            if (command.equalsIgnoreCase("Add Bazar [B.id]")){
+
+                continue;
+            }   else if(command.equalsIgnoreCase("Upgrade [B.id] [U.id]")){
+
+                continue;
+            }   else if (command.equalsIgnoreCase("remove"))
 
 
         }
