f78964d758d59fd6f8fd5694e255d79af00a1773
Aliiiqbp <aliiiqbp@gmail.com>
2018-10-25 15:53:38 +0330

    second :)))

--- TWchallenge/src/Main.java
+++ TWchallenge/src/Main.java
@@ -2,103 +2,101 @@ import java.util.*;
 
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner read = new Scanner(System.in);
+        City[] tehran = new City[2];
 
+        boolean round = false;
 
-        while (true){
+        while (true) {
 
             String command = read.nextLine();
-
+            command = command.toLowerCase();
 //End Game *_*
-            if (command.equalsIgnoreCase("Yield")){
+            if (command.equalsIgnoreCase("Yield")) {
                 break;
             }
 
 
 //Other commands like score, gill, done, attackloot.
-            if (command.equalsIgnoreCase("Done")){
+            if (command.equalsIgnoreCase("Done")) {
+                if (round == true) {
+                    round = false;
+                } else {
+                    round = true;
+                }
+                continue;
+            } else if (command.equalsIgnoreCase("See Score")) {
+                System.out.println(tehran[round].getScore());
+                continue;
+            } else if (command.equalsIgnoreCase("See Gills")) {
+                System.out.println(tehran[round].getGill());
+                continue;
+            } else if (command.matches("attack \\d")) {
 
                 continue;
-            }   else if (command.equalsIgnoreCase("See Score")){
-
-               continue;
-            }   else if (command.equalsIgnoreCase("See Gills")){
-
-                continue;
-            }   else if (command.equalsIgnoreCase("Attack [B.id]")){
-
-                continue;
-            }   else if (command.equalsIgnoreCase("Loot [B.id]")){
+            } else if (command.matches("Loot \\d")) {
 
                 continue;
             }
 
 
-
 //Add & Upgrade home
-            if (command.equalsIgnoreCase("Add Home [B.id] [numberOfFloor] [NumberOfUnit]")){
+            if (command.equalsIgnoreCase("Add Home [B.id] [numberOfFloor] [NumberOfUnit]")) {
+
 
                 continue;
-            }   else if (command.equalsIgnoreCase("Upgrade [B.id] [U.id] (floor || unit)")){
+            } else if (command.equalsIgnoreCase("Upgrade [B.id] [U.id] (floor || unit)")) {
+
 
                 continue;
             }
 
 
 //Add, Remove, Upgrade Block.
-            if (command.equalsIgnoreCase("Add Block")){
+            if (command.equalsIgnoreCase("Add Block")) {
 
                 continue;
-            }   else if (command.equalsIgnoreCase("Remove [B.id]")){
+            } else if (command.equalsIgnoreCase("Remove [B.id]")) {
 
                 continue;
-            }   else if(command.equalsIgnoreCase("Upgrade [B.id]")){
+            } else if (command.equalsIgnoreCase("Upgrade [B.id]")) {
 
                 continue;
             }
 
 
 //Add Bazaar, Army ,Defense
-            if (command.equalsIgnoreCase("Add Bazar [B.id]")){
+            if (command.equalsIgnoreCase("Add Bazar [B.id]")) {
 
                 continue;
-            }   else if(command.equalsIgnoreCase("Add Army [B.id]")){
+            } else if (command.equalsIgnoreCase("Add Army [B.id]")) {
 
                 continue;
-            }   else if (command.equalsIgnoreCase("Add Defense [B.id]")){
+            } else if (command.equalsIgnoreCase("Add Defense [B.id]")) {
 
                 continue;
             }
-
 
 
 //Upgrade [B.id][U,id]
-            if (command.equalsIgnoreCase("upgrade [B.id] [U.id]")){
+            if (command.equalsIgnoreCase("upgrade [B.id] [U.id]")) {
 
                 //army, defense, bazaar getType()
 
                 continue;
-            }   else if (command.equalsIgnoreCase("remove [B.id] [U.id]")){
+            } else if (command.equalsIgnoreCase("remove [B.id] [U.id]")) {
 
                 //army, defense, bazaar getType()
 
                 continue;
             }
-
-
-
-
-
-
-
-
 
 
         }
     }
-
 
 
 }
