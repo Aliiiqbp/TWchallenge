41aa40d6d0b972be40d87a6351c57619f712b719
ashkanlarni <ash.larni@gmail.com>
2018-10-25 15:25:11 +0330

    Bazaar added.

---
+++ TWchallenge/src/Bazaar.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Bazaar {
+    private int level;
+    private int unitID;
+    private ArrayList<Person> personArr = new ArrayList<Person>();
+    private int day;
+    private int coefficiency;
+    private int employee;
+    private int score;
+    private int gill;
+    public void upgradeBazaar(){
+
+    }
+
+    public int getScore(){
+        return score;
+    }
+    public int getGill(){
+        return gill;
+    }
+
+    public int getUnitID() {
+        return unitID;
+    }
+}
